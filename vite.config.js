ffa2d0d5e02707c022744277952af0c0fd7626f5
DianWardana <rizkidian212@gmail.com>
2026-08-09 06:18:40 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// BASE_PATH diisi otomatis oleh GitHub Actions saat deploy.
-// Kalau dijalankan lokal (npm run dev), nilainya '/'.
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_PATH || '/',
+  base: '/portopolio/',
+
   build: {
     outDir: 'dist',
     assetsInlineLimit: 2048,
   },
-})
-export default defineConfig({
-  base: '/portopolio/',
 })
